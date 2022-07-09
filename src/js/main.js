5be2fa3b3ad0869d662076bb402d55170e6d8120
Dariya Maslyukova <darija0207@ukr.net>
2022-07-10 00:55:50 +0300

    Mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,39 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function(event) {
 
+  toggleMobileMenu();
+  searchMove();
 
-  //-----------------------Mobile Menu
-  $(document).on('click', '.menu-toggle__btn', function(e) {
-    e.preventDefault();
-    $(this).toggleClass('opened');
-    $('.nav-sections-items').toggleClass('opened');
-    $('body').toggleClass('overflow');
+  window.addEventListener('resize', function(){
+      searchMove();
   });
-
 });
+
+//----------------------Search move to another place
+function searchMove() {
+  const search = document.querySelector('.search');
+  const leftColumn = document.querySelector('.two-columns__left');
+  const rightColumn = document.querySelector('.two-columns__right');
+  const isSearchInLeft = leftColumn.querySelector(search.className);
+  const isSearchInRight = rightColumn.querySelector(search.className);
+
+
+  if (window.innerWidth < 992) {
+    !isSearchInLeft ? leftColumn.prepend(search) : leftColumn.removeChild(search);
+  }
+
+  if (window.innerWidth > 991) {
+    !isSearchInRight ? rightColumn.prepend(search) : rightColumn.removeChild(search);
+  }
+
+}
+
+//-----------------------Toggle Mobile Menu
+function toggleMobileMenu() {
+  const menu = document.querySelector(".menu-toggle__btn");
+
+  menu.addEventListener("click", e => {
+    e.preventDefault();
+    menu.classList.toggle("opened");
+  }, false);
+}
+
